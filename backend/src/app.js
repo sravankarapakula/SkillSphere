37b627ba26422bd6333c24a5129df88b2fa1ebe8
SRAVAN KUMAR KARAPAKULA <sravankarapakula2006@gmail.com>
2026-05-10 11:04:03 +0530

    first commit

---
+++ backend/src/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "SkillSphere API Running"
+    });
+});
+
+module.exports = app;
---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = require("./app");
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("MongoDB Connected");
+
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+})
+.catch((err) => {
+    console.log(err);
+});
